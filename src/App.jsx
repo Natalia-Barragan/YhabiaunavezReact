ecaa3ba2c0b7e3021a353ed128f923f1bc177675
Natalia Barragan <natalia.barragannb@gmail.com>
2023-09-27 23:15:16 -0300

    PreEntrega1+Barragan

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import MensajeHeader from "./components/ItemListContainer";
+import NavBar from "./components/NavBar";
+
+function App() {
+  return (
+    <div>
+      <header>
+          <><MensajeHeader title="bienvenidos a YHABIA UNA VEZ" /></>
+          <><NavBar /></>
+      </header>
+    </div>
+  );
+}
+
+export default App;
